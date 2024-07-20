6119560aad4975f016b4fb843a465d98477eca34
Rogan <quangenw@gmail.com>
2024-07-20 16:18:55 +0800

    feat: load predefined data from json

--- starters/metadata-starter/src/main/java/info/openmeta/starter/metadata/service/SysPreDataService.java
+++ starters/metadata-starter/src/main/java/info/openmeta/starter/metadata/service/SysPreDataService.java
@@ -2,6 +2,7 @@ package info.openmeta.starter.metadata.service;
 
 import info.openmeta.framework.orm.service.EntityService;
 import info.openmeta.starter.metadata.entity.SysPreData;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -19,4 +20,25 @@ public interface SysPreDataService extends EntityService<SysPreData, Long> {
      * @param fileNames List of relative directory data file names to load
      */
     void loadPredefinedData(List<String> fileNames);
+
+    /**
+     * Loads predefined data from a given multipart file.
+     * <p>
+     * This method processes the provided multipart file to load predefined data
+     * into the system. The file is expected to be in a format that is recognized
+     * by the implementation, such as CSV, JSON, or XML.
+     * </p>
+     * <p>
+     * The method performs necessary validations and error handling to ensure the
+     * data integrity and consistency. Any issues encountered during the file
+     * processing are logged appropriately, and relevant exceptions are thrown to
+     * inform the caller about the specific problems.
+     * </p>
+     *
+     * @param file the multipart file containing the predefined data to be loaded
+     *             into the system. The file should not be null and must contain
+     *             valid data as per the required format.
+     */
+    void loadPredefinedData(MultipartFile file);
+
 }
