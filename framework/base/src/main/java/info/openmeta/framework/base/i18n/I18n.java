8bf8c4a31b6dd37ccba378d39443b0687c9e4a65
Rogan <quangenw@gmail.com>
2024-09-03 19:49:27 +0800

    Fix: Change from throwing an exception to a warn log when no translation exists for the specified language

--- framework/base/src/main/java/info/openmeta/framework/base/i18n/I18n.java
+++ framework/base/src/main/java/info/openmeta/framework/base/i18n/I18n.java
@@ -3,7 +3,7 @@ package info.openmeta.framework.base.i18n;
 import info.openmeta.framework.base.constant.BaseConstant;
 import info.openmeta.framework.base.context.Context;
 import info.openmeta.framework.base.context.ContextHolder;
-import info.openmeta.framework.base.utils.Assert;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.text.MessageFormat;
@@ -22,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
  *      I18n.get("Hello World!", Locale.SIMPLIFIED_CHINESE);
  *      I18n.get("Hello World! {0} {1}", firstName, lastName);
  */
+@Slf4j
 public class I18n {
 
     /** Language code to translation map: {languageCode: {original: translation}} */
@@ -29,28 +30,52 @@ public class I18n {
 
     /**
      * Get translation by original according to current context with or without parameters to format.
+     * The original content will be returned if there are no translations defined.
+     *
+     * @param original the original text to be translated
+     * @param args optional arguments to format the translation
+     * @return the translated text, or the original text if no translation is found
      */
     public static String get(String original, Object... args){
         Context context = ContextHolder.getContext();
-        if (BaseConstant.DEFAULT_LANGUAGE.equals(context.getLanguage())) {
+        String languageCode = context.getLanguage().getLanguage();
+        if (BaseConstant.DEFAULT_LANGUAGE.equals(context.getLanguage())
+                || !MESSAGE_TRANSLATIONS.containsKey(languageCode)) {
+            log.warn("Language {} does not has any translation", languageCode);
             return MessageFormat.format(original, args);
         } else {
-            return get(context.getLanguage(), original, args);
+            return getByLanguage(languageCode, original, args);
         }
     }
 
     /**
      * Get translation by original according to specified language with or without parameters to format.
      * The original will be treated as the default translation if it has no translation.
+     *
+     * @param languageCode the language code to get the translation for
+     * @param original the original text to be translated
+     * @param args optional arguments to format the translation
+     * @return the translated text, or the original text if no translation is found
      */
-    public static String get(Locale language, String original, Object... args){
-        String languageCode = language.getLanguage();
-        Assert.isTrue(MESSAGE_TRANSLATIONS.containsKey(languageCode), "Language not supported: {0}", languageCode);
+    public static String getByLanguage(String languageCode, String original, Object... args){
         if (StringUtils.isBlank(original)) {
             return StringUtils.EMPTY;
         }
         String translation =  MESSAGE_TRANSLATIONS.get(languageCode).getOrDefault(original, original);
         return MessageFormat.format(translation, args);
+    }
+
+    /**
+     * Get translation by original according to specified language with or without parameters to format.
+     * The original will be treated as the default translation if it has no translation.
+     *
+     * @param language the locale to get the translation for
+     * @param original the original text to be translated
+     * @param args optional arguments to format the translation
+     * @return the translated text, or the original text if no translation is found
+     */
+    public static String getByLanguage(Locale language, String original, Object... args){
+        return getByLanguage(language.getLanguage(), original, args);
     }
 
     public static void put(String languageCode, List<TranslationItem> translationItems) {
