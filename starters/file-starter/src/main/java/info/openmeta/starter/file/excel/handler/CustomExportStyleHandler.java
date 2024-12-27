3c438c1066ed329049238c0b2c1fa79ea7c04e57
huangjinghao <huang.jinghao@fescoadecco.com>
2024-12-27 17:50:35 +0800

    修改导出模版的样式

---
+++ starters/file-starter/src/main/java/info/openmeta/starter/file/excel/handler/CustomExportStyleHandler.java
@@ -0,0 +1,73 @@
+package info.openmeta.starter.file.excel.handler;
+
+import com.alibaba.excel.write.handler.CellWriteHandler;
+import com.alibaba.excel.write.handler.context.CellWriteHandlerContext;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * custom export style handler
+ * head color and font style
+ */
+public class CustomExportStyleHandler implements CellWriteHandler {
+
+    /**
+     * Mix styles to reduce repetitive generation styles
+     */
+    private Map<String, CellStyle> styleCache = new HashMap<>();
+
+    @Override
+    public void afterCellDispose(CellWriteHandlerContext context) {
+        Sheet sheet = context.getWriteSheetHolder().getSheet();
+        Workbook workbook = context.getWriteWorkbookHolder().getWorkbook();
+
+        sheet.setDefaultColumnWidth(25);
+
+        // Set the global font to Microsoft Yahei
+        Font defaultFont = workbook.createFont();
+        defaultFont.setFontName("微软雅黑");
+        defaultFont.setFontHeightInPoints((short) 11);
+        CellStyle defaultStyle = workbook.createCellStyle();
+        defaultStyle.setFont(defaultFont);
+        styleCache.put("default", defaultStyle);
+
+        // Sets the header row style
+        Row titleRow = sheet.getRow(0);
+        titleRow.setHeightInPoints((short) 30);
+        Font titleFont = workbook.createFont();
+        titleFont.setBold(true);
+        titleFont.setFontName("微软雅黑");
+        titleFont.setFontHeightInPoints((short) 11);
+        titleFont.setColor(IndexedColors.WHITE.getIndex());
+        XSSFCellStyle titleStyle = (XSSFCellStyle) workbook.createCellStyle();
+        titleStyle.setFont(titleFont);
+        titleStyle.setFillForegroundColor(IndexedColors.DARK_TEAL.getIndex());
+        titleStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        titleStyle.setAlignment(HorizontalAlignment.CENTER);
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleCache.put("title", titleStyle);
+
+        // Apply the default style to header row
+        for (int i = 0; i < titleRow.getLastCellNum(); i++) {
+            Cell cell = titleRow.getCell(i);
+            cell.setCellStyle(titleStyle);
+        }
+
+        // Apply the default style to all cells
+        for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+            Row row = sheet.getRow(rowIndex);
+            if (row != null) {
+                for (int cellIndex = 0; cellIndex < row.getLastCellNum(); cellIndex++) {
+                    Cell cell = row.getCell(cellIndex);
+                    if (cell != null) {
+                        cell.setCellStyle(defaultStyle);
+                    }
+                }
+            }
+        }
+    }
+
+}
